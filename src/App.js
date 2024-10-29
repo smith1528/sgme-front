0cf29fd950c95628b85b5d3f6aa24058e423fbcc
Bairon <Bairon@LAPTOP-2J8I9G57>
2024-10-29 10:53:00 -0500

    Ventana perfiles de usuario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'; // CSS principal del proyecto
-import LoginRegister from './components/LoginRegister'; // Importa el componente
+import LoginRegister from './components/LoginRegister';
+import UserProfile from './components/UserProfile';
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado para alternar la vista
+
     return (
         <div className="App SGME">
-            <LoginRegister /> 
+            {isLoggedIn ? <UserProfile /> : <LoginRegister />}
+
+            {/* Botón para cambiar entre vistas (solo para pruebas, puedes manejar esto según la lógica de tu aplicación) */}
+            <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+                {isLoggedIn ? 'Logout' : 'Login'}
+            </button>
         </div>
     );
 }
